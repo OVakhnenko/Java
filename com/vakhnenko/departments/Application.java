b4fd1c78d551d93c27d4c25234a1642e165a3bc5
OVakhnenko <oleksiy@vakhnenko.com>
2017-01-25 08:15:57 +0300

    Merge branch 'master' of D:\Idea\JavaGitHub with conflicts.

--- com/vakhnenko/departments/Application.java
+++ com/vakhnenko/departments/Application.java
@@ -1,4 +1,10 @@
 package com.vakhnenko.departments;
 
-class Application {
+import java.lang.reflect.Array;
+
+public class Application {
+    String command = "   create  -d  111 111 dep      "
+    String[] commands = command.split(" ");
+
+    Array.toString()
 }
