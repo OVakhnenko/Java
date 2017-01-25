e2bc5d77c6e6a8fef191239d01a3995b31e50a8f
OVakhnenko <oleksiy@vakhnenko.com>
2017-01-25 14:32:39 +0300

    ...

---
+++ com/golovach/courses/FibonacciExpression.java
@@ -0,0 +1,55 @@
+package com.golovach.courses;
+
+// ((1+(0+1))+((0+1)+(1+(0+1))))
+//
+// 0
+// 1
+// 0+1
+// 1+(0+1)
+// ((0+1)+1+(0+1))
+// ((1+(0+1))+((0+1)+(1+(0+1))))
+//
+public class FibonacciExpression {
+    public static void main(String[] args) {
+        System.out.println("((1+(0+1))+((0+1)+(1+(0+1))))");
+        f(5);
+    }
+
+    public static int f(int x) {
+        if (x < 2)
+            return abc(x);
+        else
+            return a(x) + f(x - 2) + b(x) + f(x - 1) + c(x);
+    }
+
+    public static int abc(int x) {
+        System.out.print(x);
+        return x;
+    }
+
+    public static int a(int x) {
+        //System.out.print("(");
+        System.out.print(x);
+        if (x == 2)
+            System.out.print("(");
+        else if (!(x % 2 == 0)) {
+            System.out.print("(");
+        }
+        return 0;
+    }
+
+    public static int b(int x) {
+        System.out.print("+");
+        return 0;
+    }
+
+    public static int c(int x) {
+        if (x == 2)
+            System.out.print(")");
+        else if (!(x % 2 == 0)) {
+            System.out.print(")");
+        }
+        //System.out.print(")");
+        return 0;
+    }
+}
