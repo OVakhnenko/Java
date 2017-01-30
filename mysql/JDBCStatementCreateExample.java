fcb64d846dac439eaef93f16b3a3fb9b65f88d5e
OVakhnenko <oleksiy@vakhnenko.com>
2017-01-30 18:37:42 +0300

    ...

---
+++ mysql/JDBCStatementCreateExample.java
@@ -0,0 +1,89 @@
+package mysql;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCStatementCreateExample {
+
+    private static final String DB_DRIVER = "com.mysql.jdbc.Driver"; //"oracle.jdbc.driver.OracleDriver";
+    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/departments";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "oJ0K58fE29kZhgXcQQV7";
+
+    public static void main(String[] argv) {
+
+        try {
+            createDbUserTable();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void createDbUserTable() throws SQLException {
+
+        Connection dbConnection = null;
+        Statement statement = null;
+
+        String dropTableSQL = "DROP TABLE TEST";
+        String createTableSQL = "CREATE TABLE TEST ("
+                + "id int(11) NOT NULL, "
+                + "name varchar(50) NOT NULL, "
+                + "author varchar(50) NOT NULL, "
+                + "PRIMARY KEY (id) "
+                + ")ENGINE = InnoDB";
+        String insertDataToTable1 = "INSERT INTO TEST "
+                + "(id, name, author) VALUES (1, 'Effective Java', 'Joshua Bloch')";
+        String insertDataToTable2 = "INSERT INTO TEST "
+                + "(id, name, author) VALUES (2, 'Java Concurrency in Practice', 'Brian Goetz')";
+        try {
+            dbConnection = getDBConnection();
+            statement = dbConnection.createStatement();
+
+            System.out.println(dropTableSQL);
+            statement.execute(dropTableSQL);
+
+            System.out.println(createTableSQL);
+            statement.execute(createTableSQL);
+
+            System.out.println(insertDataToTable1);
+            statement.execute(insertDataToTable1);
+            System.out.println(insertDataToTable2);
+            statement.execute(insertDataToTable2);
+
+            System.out.println("Table \"TEST\" is created!");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (dbConnection != null) {
+                dbConnection.close();
+            }
+        }
+    }
+
+    private static Connection getDBConnection() {
+
+        Connection dbConnection = null;
+
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            dbConnection = DriverManager.getConnection(
+                    DB_CONNECTION, DB_USER, DB_PASSWORD);
+            return dbConnection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return dbConnection;
+    }
+}
