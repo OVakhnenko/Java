19d5f833f849e86613598d21b24d6f30f8a08b32
Aleksey <oleksiy@vakhnenko.com>
2017-03-12 19:08:48 +0300

    ...

--- paramstr/Read.java
+++ paramstr/Read.java
@@ -3,11 +3,17 @@ package paramstr;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Properties;
 
 public class Read {
     public static void main(String[] args) throws IOException {
         Properties p = new Properties();
+
+        for (Field field : p.getClass().getFields()) {
+            System.out.println(field.getName());
+            field.setAccessible(true);
+        }
 
         p.load(new FileInputStream("paramstr.ini"));
         System.out.println(p);
